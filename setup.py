6606f4968d3f875a98755a697e25e3e3b38d1854
jordij <jordi.joan@gmail.com>
2015-07-07 16:14:00 +1200

    Type fixed

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtailgmaps',
-    version='0.2.1',
+    version='0.2.2',
     packages=['wagtailgmaps'],
     include_package_data=True,
     license='BSD License',
