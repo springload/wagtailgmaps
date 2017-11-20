943ecb1c8746cdc9bc3d8977bf9ea7526bb1c84e
Loic Teixeira <loic@springload.co.nz>
2017-11-20 15:12:47 +1000

    Linting

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 from wagtailgmaps import __version__
 
