90d919fd96ca9a239fbd979015c4d0de46b9d935
Loic Teixeira <loic@springload.co.nz>
2018-02-27 10:31:27 +1100

    Add tests

---
+++ tests/test_edit_handlers.py
@@ -0,0 +1,68 @@
+import unittest
+
+from django.conf import settings
+from django.test import SimpleTestCase
+
+from wagtailgmaps.edit_handlers import MapFieldPanel
+from wagtailgmaps.widgets import MapInput
+
+
+class EditHandlersTestCase(SimpleTestCase):
+    def test_init_with_defaults(self):
+        panel = MapFieldPanel('field-name')
+
+        self.assertEqual(panel.default_centre, settings.WAGTAIL_ADDRESS_MAP_CENTER)
+        self.assertEqual(panel.zoom, 8)
+        self.assertEqual(panel.latlng, False)
+
+    def test_init_with_values(self):
+        panel = MapFieldPanel(
+            'field-name',
+            centre='somewhere',
+            zoom=0,
+            latlng=True,
+        )
+
+        self.assertEqual(panel.default_centre, 'somewhere')
+        self.assertEqual(panel.zoom, 0)
+        self.assertEqual(panel.latlng, True)
+
+    def test_clone(self):
+        panel = MapFieldPanel(
+            'field-name',
+            centre='somewhere',
+            zoom=0,
+            latlng=True,
+        )
+        clone = panel.clone()
+
+        self.assertEqual(panel.field_name, clone.field_name)
+        self.assertEqual(panel.default_centre, clone.default_centre)
+        self.assertEqual(panel.zoom, clone.zoom)
+        self.assertEqual(panel.latlng, clone.latlng)
+        self.assertEqual(panel.map_id, clone.map_id)
+
+    @unittest.skip('TODO: Bind the panel to the model for the test to succeed.')
+    def test_classes(self):
+        panel = MapFieldPanel(
+            'field-name',
+            centre='somewhere',
+            zoom=0,
+            latlng=True,
+        )
+        classes = panel.classes()
+
+        self.assertIn('wagtailgmap', classes)
+
+    def test_widget_overrides(self):
+        field_name = 'field-name'
+        panel = MapFieldPanel(
+            field_name,
+            centre='somewhere',
+            zoom=0,
+            latlng=True,
+        )
+        widget_overrides = panel.widget_overrides()
+
+        self.assertIn(field_name, widget_overrides)
+        self.assertIsInstance(widget_overrides[field_name], MapInput)
